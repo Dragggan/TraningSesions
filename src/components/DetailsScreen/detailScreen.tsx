72757877c71a989a37d95b38cfef29c0e00a091c
Djuza, Dragan <dragan.djuza@centogene.com>
2022-01-22 00:20:05 +0100

    wip, dynamic component

--- src/components/DetailsScreen/detailScreen.tsx
+++ src/components/DetailsScreen/detailScreen.tsx
@@ -5,9 +5,12 @@ import volleyball from '../../icons/volleyball.svg'
 import chevronleft from '../../icons/chevron-left.svg'
 import batterycharging from '../../icons/battery-charging.svg'
 import batterycharging_green from '../../icons/batterycharging_green.svg'
+import batery_charger_yellow from '../../icons/batery_charger_yellow.svg'
 import ok from '../../icons/ok.svg'
 import text from '../../icons/text.svg'
 import topfit from '../../icons/topfit.svg'
+import speedometer from '../../icons/speedometer.svg'
+import intense from '../../icons/intense.svg'
 
 interface CustomizedState {
     id: string
@@ -27,23 +30,13 @@ export const DetailsScreen: FC = () => {
     const state = location.state as CustomizedState;
     const navigate = useNavigate()
 
-    console.log('%c  location.state==> ', 'color:red;font-size:12px;', location.state);
-
-    // date: "2022-01-24"
-    // description: "massage"
-    // duration: 90
-    // endTime: "20:30"
-    // favorite: false
-    // id: 5
-    // restLevel: 1
-    // startTime: "19:00"
-    // type: "recovery"
-
-
-
     const { type, date, startTime, endTime, duration } = state;
+
+    const dynamic_class_type = () => {
+        return type === 'training' ? 'details_type' : "details_type_recovery"
+    }
     return type === 'training' ? (
-        <div className="container_wrapper">
+        <div className="container_wrapper_green">
             <div className="icon_wrapper">
                 <img src={chevronleft} alt="chevronleft" className='chevronleft' onClick={() => navigate('/')} />
                 <img src={volleyball} alt="volleyball" className='volleyball' />
@@ -51,24 +44,29 @@ export const DetailsScreen: FC = () => {
             {/* headlines..... */}
             <div className='midle_section'>
                 <h1 className='details_type'>{type}</h1>
+                {/* todo format date */}
                 <h2 className='time'>{date}</h2>
                 <div className='time'>
-                    <span >{startTime}</span>-<span>{endTime}</span>&#8226;<span className='duration_time'>{duration.toFixed()}</span></div>
+                    <span >{startTime}</span> - <span>{endTime}</span>&#8226;<span className='duration_time'>{duration.toFixed()}</span></div>
             </div>
             {/* ---------------------------- */}
-            <div>
-                <img src={batterycharging} alt="batterycharging" className='batterycharging' />
+            <div className='batery_wrapper'>
+                <img src={batery_charger_yellow} alt="batery_charger_yellow" className='batery_charger_yellow' />
                 I felt  <img src={ok} alt="ok" className='ok' /> before the training session.
+            </div>
+            <div className='speed_wrapper'>
+                <img src={speedometer} alt="speedometer" className='speedometer' /> I perceived the training as <img src={intense} alt="intense" className='intense' />
+                The total load was 240.
             </div>
         </div >
     ) : (
-        <div className="container_wrapper">
+        <div className="container_wrapper_blue">
             <div className="icon_wrapper">
-                <img src={chevronleft} alt="chevronleft" className='chevronleft' />
+                <img src={chevronleft} alt="chevronleft" className='chevronleft' onClick={() => navigate('/')} />
                 <img src={batterycharging} alt="batterycharging" className='batterycharging_big' />
             </div>
             {/* headlines..... */}
-            <h3 className='details_type'>{state.type}</h3>
+            <h3 className={dynamic_class_type()}>{state.type}</h3>
             <h4>{state.date}</h4>
             <span>{startTime}</span>-<span>{endTime}</span>.<span>{duration}</span>
             {/* ---------------------------- */}
@@ -76,7 +74,6 @@ export const DetailsScreen: FC = () => {
                 <img src={text} alt="text" className='text' /> Warm bath.
                 <span>
                     <img src={batterycharging_green} alt="batterycharging" className='batterycharging_green' />I felt</span><img src={topfit} alt="topfit" className='topfit' /> after the recovery session.
-
             </div>
         </div>
     )
