4ccc0c48f2ca4b94e2c3cc0f2303f325dadddc04
Djuza, Dragan <dragan.djuza@centogene.com>
2022-01-21 23:09:04 +0100

    css details page started

--- src/components/DetailsScreen/detailScreen.tsx
+++ src/components/DetailsScreen/detailScreen.tsx
@@ -1,8 +1,8 @@
 import React, { FC, useState, useEffect } from 'react'
 import { useLocation, Link } from 'react-router-dom';
-import { Location } from "history";
-
-
+import './detailScreen.css'
+import volleyball from '../../icons/volleyball.svg'
+import chevronleft from '../../icons/chevron-left.svg'
 
 interface CustomizedState {
     id: string
@@ -22,21 +22,30 @@ export const DetailsScreen: FC = () => {
     const state = location.state as CustomizedState;
     console.log('%c  location.state==> ', 'color:red;font-size:12px;', location.state);
 
-    if (state) {
-        return (
-            <>
-                <h1>{state.id}</h1>
-                <h1>{state.date}</h1>
-                <h1>{state.description}</h1>
-                <h1>{state.duration}</h1>
-                <h1>{state.favorite}</h1>
-                <nav>
-                    <Link to="/">go back</Link>
-                </nav>
-            </>
-        )
-    }
+    // if (state) {
+    //     return (
+    //         <>
+    //             <h1>{state.id}</h1>
+    //             <h1>{state.date}</h1>
+    //             <h1>{state.description}</h1>
+    //             <h1>{state.duration}</h1>
+    //             <h1>{state.favorite}</h1>
+    //             <nav>
+    //                 <Link to="/">go back</Link>
+    //             </nav>
+    //         </>
+    //     )
+    // }
+    // return (
+    //     <h2>error caling state</h2>
+    // )
     return (
-        <h2>error caling state</h2>
+        <div className="container_wrapper">
+            <div className="icon_wrapper">
+                <img src={chevronleft} alt="chevronleft" className='chevronleft' />
+                <img src={volleyball} alt="volleyball" className='volleyball' />
+            </div>
+
+        </div>
     )
 }
